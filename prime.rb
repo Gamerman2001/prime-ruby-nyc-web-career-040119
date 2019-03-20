7315624d49a26c5204814d212df9fb58e7df13f5
Troy Gooden <tro12185@gmail.com>
2019-03-20 22:35:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -18,10 +18,12 @@
 def prime?(int)
   if int < 0 || int == 1 || int == 0 
     false
-  else 
+  elsif 
     (2...int).each do |findprime|
       int % findprime == 0
         return false
+      else 
+        return true
     end 
   end 
 end 
