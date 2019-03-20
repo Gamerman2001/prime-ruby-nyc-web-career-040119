daa011af9bd2e7e3a660e4712f4ab383615140e0
Troy Gooden <tro12185@gmail.com>
2019-03-20 22:28:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -24,3 +24,12 @@
 #     end 
 #   end 
 # end 
+
+if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
