f96f824e16cc8142e6dc2744851de732f0a89663
Troy Gooden <tro12185@gmail.com>
2019-03-20 21:52:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+# # Add  code here!
+# def prime?(int)
+#   prime_factors = [2, 3, 5, 7, 11, 13, 17]
+#   # if int == 2 
+#   #   true 
+#   # elsif 
+#   #   prime_factors.each do |pf|
+#   #     int % pf == 0 ? false : true
+#   #   end 
+#   # end 
+#   prime_factors.each do |pf|
+#     int % pf == 0 ? false : true 
+#   end 
+# end 
+
+def prime?(int)
+  # prime_factors = [2, 3, 5, 7, 11, 13, 17]
+  if int  > 1 && int == 2
+     return true 
+   else 
+  for i in (2..int)
+    if int % i == 0 
+    return false
+  else
+    return true
+    end
+  end 
+  end
+  false
+end 
